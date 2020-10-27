659b0dddacaa86720f76086cff8b0607e3a2cff7
tejaswini <tejaswini@abc.com>
2020-10-27 11:07:41 +0530

    new code added

--- MyJenkins_Maven/src/main/java/com/data/App.java
+++ MyJenkins_Maven/src/main/java/com/data/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to eclipse jenkins");
+		System.out.println("Welcome to eclipse jenkins1234");
 
 	}
 
